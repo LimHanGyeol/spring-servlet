c56cd19f40e12fc6862b658d8693ed2246644a7c
LimHanGyeol <dlagksruf19@naver.com>
2020-03-10 19:15:32 +0900

    Servlet1. Spring MVC 연동

---
+++ src/main/java/me/tommy/HelloService.java
@@ -0,0 +1,13 @@
+package me.tommy;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getName() {
+        return "tommy Lim";
+    }
+
+
+}
